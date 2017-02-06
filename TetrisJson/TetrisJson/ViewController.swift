e8c22243d9b640f970d8d140039fe71b75ff8157
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2017-02-06 19:08:01 +0100

    added tableViewController-class and table view

--- TetrisJson/TetrisJson/ViewController.swift
+++ TetrisJson/TetrisJson/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    // MARK: - Setup Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +37,32 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    // MARK: - Table View
 
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //        let object = objects[(indexPath as NSIndexPath).row]
+        //        cell.displayName!.text = "\(Route.getDisplayName(route: object)) - \(Route.getPriceString(route: object))"
+        //        if let request = Route.getProviderIconRequestURL(route: object) {
+        //            cell.providerIcon.loadRequest(request)
+        //        }
+        cell.textLabel!.text = "asds"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
 }
 
