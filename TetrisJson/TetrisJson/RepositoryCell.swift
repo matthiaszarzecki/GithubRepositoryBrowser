75f8726031da971b2aa5db3ee84e735a9e66e57d
Matthias Zarzecki <matthias@promptus-partners.de>
2017-02-07 07:29:09 +0100

    added custom display cell

---
+++ TetrisJson/TetrisJson/RepositoryCell.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryCell.swift
+//  TetrisJson
+//
+//  Created by Matthias Zarzecki on 07/02/2017.
+//  Copyright © 2017 Matthias Zarzecki. All rights reserved.
+//
+
+import Foundation
